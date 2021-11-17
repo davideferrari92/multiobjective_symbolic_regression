e43d9a20d6662527aae41b292cc66e18e97f2b75
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2021-11-17 16:54:51 +0000

    Adds non-weighted option in WMSE fitness function
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

--- symbolic_regression/multiobjective/fitness.py
+++ symbolic_regression/multiobjective/fitness.py
@@ -7,12 +7,14 @@ from symbolic_regression.Program import Program
 def wmse(program: Program,
          data: Union[pd.DataFrame, pd.Series],
          target: str,
-         weights: str) -> float:
+         weights: str = None) -> float:
     """ Evaluates the weighted mean squared error
     """
 
     pred = program.evaluate(data=data)
 
-    wmse = (((pred - data[target]) ** 2) * data[weights]).mean()
-
+    if weights:
+         wmse = (((pred - data[target]) ** 2) * data[weights]).mean()
+    else:
+         wmse = (((pred - data[target]) ** 2)).mean()
     return wmse
