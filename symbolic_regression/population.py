b4ee6e64aef08f50810630dd09b3a38ea96a2718
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2021-12-01 15:04:09 +0000

    Add the functions to generate one and n programs within a population
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

---
+++ symbolic_regression/population.py
@@ -0,0 +1,51 @@
+from joblib import Parallel, delayed
+from symbolic_regression.Program import Program
+
+
+def generate_population(features,
+                        operations,
+                        fitness,
+                        data,
+                        target,
+                        weights,
+                        const_range,
+                        parsimony=0.90,
+                        parsimony_decay=0.90):
+    """
+    """
+    p = Program(
+        features=features,
+        operations=operations,
+        const_range=const_range
+    )
+
+    p.init_program(parsimony=parsimony, parsimony_decay=parsimony_decay)
+
+    from symbolic_regression.multiobjective.training import eval_fitness
+
+    p.fitness = eval_fitness(fitness=fitness, program=p,
+                             data=data, target=target, weights=weights)
+
+    return p
+
+
+def generate_population_n(n,
+                          features,
+                          operations,
+                          fitness,
+                          data,
+                          target,
+                          weights,
+                          const_range,
+                          parsimony,
+                          parsimony_decay):
+    """
+    """
+
+    return Parallel(n_jobs=-1)(
+        delayed(generate_population)(
+            features, operations, fitness, data, target,
+            weights, const_range, parsimony=parsimony,
+            parsimony_decay=parsimony_decay
+        ) for _ in range(n)
+    )
