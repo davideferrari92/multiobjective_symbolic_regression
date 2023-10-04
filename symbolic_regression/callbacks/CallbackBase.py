464aa3ca9b2f6e669459742a0b700fffa5aae4b6
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2023-10-04 11:57:00 +0100

    Add CallbackBase
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

---
+++ symbolic_regression/callbacks/CallbackBase.py
@@ -0,0 +1,58 @@
+from abc import abstractmethod
+
+
+class MOSRCallbackBase:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.kwargs})"
+
+    @abstractmethod
+    def on_initialization_start(self):
+        pass
+
+    @abstractmethod
+    def on_initialization_end(self):
+        pass
+
+    @abstractmethod
+    def on_generation_start(self):
+        pass
+
+    @abstractmethod
+    def on_generation_end(self):
+        pass
+
+    @abstractmethod
+    def on_offspring_generation_start(self):
+        pass
+
+    @abstractmethod
+    def on_offspring_generation_end(self):
+        pass
+
+    @abstractmethod
+    def on_refill_start(self):
+        pass
+
+    @abstractmethod
+    def on_refill_end(self):
+        pass
+
+    @abstractmethod
+    def on_stats_calculation_start(self):
+        pass
+
+    @abstractmethod
+    def on_stats_calculation_end(self):
+        pass
+
+    @abstractmethod
+    def on_convergence(self):
+        pass
+
+    @abstractmethod
+    def on_training_completed(self):
+        pass
