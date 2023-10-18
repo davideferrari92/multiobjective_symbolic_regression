daf101d1afa0725284cc6804858ea06c2c66410e
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2023-10-18 12:38:16 +0100

    Fix CallbackBase methods to support knwargs
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

--- symbolic_regression/callbacks/CallbackBase.py
+++ symbolic_regression/callbacks/CallbackBase.py
@@ -10,53 +10,53 @@ class MOSRCallbackBase:
         return f"{self.__class__.__name__}({self.kwargs})"
 
     @abstractmethod
-    def on_callback_set_init(self):
+    def on_callback_set_init(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_initialization_start(self):
+    def on_initialization_start(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_initialization_end(self):
+    def on_initialization_end(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_generation_start(self):
+    def on_generation_start(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_generation_end(self):
+    def on_generation_end(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_offspring_generation_start(self):
+    def on_offspring_generation_start(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_offspring_generation_end(self):
+    def on_offspring_generation_end(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_refill_start(self):
+    def on_refill_start(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_refill_end(self):
+    def on_refill_end(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_stats_calculation_start(self):
+    def on_pareto_front_computation_start(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_stats_calculation_end(self):
+    def on_pareto_front_computation_end(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_convergence(self):
+    def on_convergence(self, **kwargs):
         pass
 
     @abstractmethod
-    def on_training_completed(self):
+    def on_training_completed(self, **kwargs):
         pass
