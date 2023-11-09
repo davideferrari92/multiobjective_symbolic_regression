8c21161fa3f586322cdf19153c928275a9076eb6
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2023-11-09 09:54:01 +0000

    Add MOSRHistory to the callbacks
    Use this callback to set how frequently to save the best
    models during the training
    
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

--- symbolic_regression/callbacks/CallbackStatistics.py
+++ symbolic_regression/callbacks/CallbackStatistics.py
@@ -1,6 +1,7 @@
+import copy
 import time
 import plotly.graph_objs as go
-from symbolic_regression.SymbolicRegressor import SymbolicRegressor
+from symbolic_regression.SymbolicRegressor import SymbolicRegressor, compress
 
 from symbolic_regression.callbacks.CallbackBase import MOSRCallbackBase
 
@@ -16,8 +17,6 @@ class MOSRStatisticsComputation(MOSRCallbackBase):
             'compute_tree_diversity', False)
         self.compute_spearman_diversity = kwargs.get(
             'compute_spearman_diversity', False)
-        
-        print(kwargs)
 
     def _hypervolume(self):
         before = time.perf_counter()
@@ -60,3 +59,61 @@ class MOSRStatisticsComputation(MOSRCallbackBase):
                 self._spearman_diversity(data)
 
         del data
+
+
+class MOSRHistory(MOSRCallbackBase):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.history_fpf_frequency = kwargs.get('history_fpf_frequency', -1)
+
+    def on_pareto_front_computation_end(self, **kwargs):
+        
+        if (self.sr.generation == 1) or \
+            (self.history_fpf_frequency == -1 and (self.sr.generation == self.sr.generations_to_train or self.sr.converged_generation)) or \
+                (self.history_fpf_frequency > 0 and self.sr.generation % self.history_fpf_frequency == 0):
+
+            val_data = kwargs.get('val_data', None)
+
+            self.sr: 'SymbolicRegressor'
+
+            if self.sr.verbose > 1:
+                print(
+                    f'Saving FPF history for generation {self.sr.generation}')
+
+            if not self.sr.first_pareto_front_history:
+                self.sr.first_pareto_front_history = dict()
+
+            self.sr.first_pareto_front_history[self.sr.generation] = compress(
+                [p for p in self.sr.first_pareto_front])
+
+            if not self.sr.best_history.get('training'):
+                self.sr.best_history['training'] = dict()
+            if not self.sr.best_history['training'].get(self.sr.generation):
+                self.sr.best_history['training'][self.sr.generation] = dict()
+
+            if val_data is not None and not self.sr.best_history.get('validation'):
+                self.sr.best_history['validation'] = dict()
+            if val_data is not None and not self.sr.best_history['validation'].get(self.sr.generation):
+                self.sr.best_history['validation'][self.sr.generation] = dict()
+
+            for fitness in self.sr.fitness_functions:
+                if fitness.minimize:
+                    best_p = min([p for p in self.sr.first_pareto_front if p.is_valid],
+                                 key=lambda obj: obj.fitness.get(fitness.label, +float('inf')))
+                    if val_data is not None:
+                        best_p_val = min([p for p in self.sr.first_pareto_front if p.is_valid],
+                                         key=lambda obj: obj.fitness_validation.get(fitness.label, +float('inf')))
+                else:
+                    best_p = max([p for p in self.sr.first_pareto_front if p.is_valid],
+                                 key=lambda obj: obj.fitness.get(fitness.label, -float('inf')))
+                    if val_data is not None:
+                        best_p_val = max([p for p in self.sr.first_pareto_front if p.is_valid],
+                                         key=lambda obj: obj.fitness_validation.get(fitness.label, -float('inf')))
+
+                self.sr.best_history['training'][self.sr.generation][fitness.label] = compress(
+                    best_p)
+                if val_data is not None:
+                    self.sr.best_history['validation'][self.sr.generation][fitness.label] = compress(
+                        best_p_val)
