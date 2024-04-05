2b9e9f4dbb986dc689dbf2ccfe95c7ca5932b5d6
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2024-04-05 18:14:33 +0100

    Add threshold handling in SymbolicEnsembler predict
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

--- symbolic_regression/Ensembler.py
+++ symbolic_regression/Ensembler.py
@@ -8,25 +8,57 @@ class SymbolicEnsembler:
     def __init__(self, programs_selection) -> None:
         self.programs_selection = programs_selection
 
-    def predict(self, data):
-        predictions = []
-        for program in self.programs_selection:
-            predictions.append(program.predict(data))
-        return np.mean(predictions, axis=0)
-    
-    def compute_fitness(self, data, fitness_functions):
-        predictions = pd.Series(self.predict(data))
+    def predict(self, data, threshold: float = None):
+        """
+        Predicts the output for the given input data using the ensemble of programs.
 
-        fitness_blueprint = {}
+        Parameters:
+        - data: Union[pd.DataFrame, pd.Series, Dict]
+            The input data for which predictions need to be made.
+        - threshold: float  (default=None)
+            The threshold value to be used for binary classification. If None, regression predictions are made.
+
+        Returns:
+        - predictions: pd.Series
+            The predicted output for the given input data.
+
+        """
+        predictions = pd.DataFrame()
+        for index, program in enumerate(self.programs_selection):
+            predictions[index] = program.predict(data)
+
+        if threshold is not None:
+            predictions = (predictions > threshold).astype(int)
+            return predictions.mean(axis=1).astype(int)
+        else:
+            return predictions.mean(axis=1)
+
+    def compute_fitness(self, data, fitness_functions):
+        """
+        Computes the fitness of the ensembler model on the given data using the specified fitness functions.
+
+        Args:
+            data: Union[pd.DataFrame, pd.Series, Dict]
+                The input data for which fitness needs to be computed.
+            fitness_functions: List[FitnessFunction]
+                A list of fitness functions to be used for computing the fitness.
+
+        Returns:
+            fitness: Dict
+                A dictionary containing the fitness values for each fitness function.
+        """
+
+        fitness = {}
 
         for fitness_function in fitness_functions:
+            predictions = pd.Series(
+                self.predict(data,
+                             threshold=fitness_function.threshold if hasattr(fitness_function, 'threshold') else None))
             try:
-                if hasattr(fitness_function, 'threshold'):
-                    predictions = np.where(predictions > fitness_function.threshold, 1, 0)
-
-                fitness_blueprint[fitness_function.label] = fitness_function.evaluate(
-                    program = None, data = data, validation = False, pred = predictions)
+                fitness[fitness_function.label] = fitness_function.evaluate(
+                    program=None, data=data, validation=False, pred=predictions)
             except Exception as e:
-                logging.warning(f'Unable to compute fitness {fitness_function.label} {e}')
-            
-        return fitness_blueprint
+                logging.warning(
+                    f'Unable to compute fitness {fitness_function.label}')
+
+        return fitness
