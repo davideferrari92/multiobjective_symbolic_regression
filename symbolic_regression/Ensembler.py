cd937d7ea02d0f89c1c56461087b31c2d70056b0
Davide Ferrari <davide.ferrari@kcl.ac.uk>
2024-04-05 11:07:33 +0100

    Add SymbolicEnsembler
    Signed-off-by: Davide Ferrari <davide.ferrari@kcl.ac.uk>

---
+++ symbolic_regression/Ensembler.py
@@ -0,0 +1,32 @@
+import logging
+import numpy as np
+import pandas as pd
+
+
+class SymbolicEnsembler:
+
+    def __init__(self, programs_election) -> None:
+        self.programs_election = programs_election
+
+    def predict(self, data):
+        predictions = []
+        for program in self.programs_election:
+            predictions.append(program.predict(data))
+        return np.mean(predictions, axis=0)
+    
+    def compute_fitness(self, data, fitness_functions):
+        predictions = pd.Series(self.predict(data))
+
+        fitness_blueprint = {}
+
+        for fitness_function in fitness_functions:
+            try:
+                if hasattr(fitness_function, 'threshold'):
+                    predictions = np.where(predictions > fitness_function.threshold, 1, 0)
+
+                fitness_blueprint[fitness_function.label] = fitness_function.evaluate(
+                    program = None, data = data, validation = False, pred = predictions)
+            except Exception as e:
+                logging.warning(f'Unable to compute fitness {fitness_function.label} {e}')
+            
+        return fitness_blueprint
